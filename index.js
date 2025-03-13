6e740131148fabdb2b0c2fed804b3516274b06d4
khadijun@DESKTOP <kardikhadija1@gmail.com>
2025-03-13 22:40:47 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+let record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+function superbowlWin(record) {
+  let win = record.find(function (object) {
+    return object.result === "W";
+  });
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+}
+
+// let Win = record.find((record) => record.result === "W");
+
+// if (Win) {
+//   return result.year;
+// } else {
+//   return undefined;
+// }
